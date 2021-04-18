3152f89674869b5232a5a8f4460e5c1f275b08b9
Armando Soto <armando29@live.com>
2021-04-18 06:24:25 +0000

    Added more changes

--- src/containers/nav.js
+++ src/containers/nav.js
@@ -64,11 +64,11 @@ export default function Nav({ progress }) {
             <div ref={containerRef} id="anchor_0" className="absolute top left corebox_5 " style={{ pointerEvents: "none" }}>
 
             </div>
-            <div className="nav row corebox_5 back_2 boxshadow_27 z_index14 center" style={{ transform: (isVisible ? "translateY(-100%)" : "translateY(0)"), pointerEvents: (isVisible ? "none" : "all") }}>
+            <div className={"nav row corebox_5 back_2  z_index14 center"+ (isVisible ? "" : " boxshadow_20") } style={{ transform: (isVisible ? "translateY(-100%)" : "translateY(0)"), pointerEvents: (isVisible ? "none" : "all") }}>
                 <div className="nav_body corebox_5  end items_center ls_30 ffam_1 f_1 f800  fore_14 " style={{ maxWidth: "84.21053vw !important" }}>
                     <div className="row center maxedcorebox_x18 wrap nice_nav_0">
                         {
-                            ["Home", "Projects", "About", "Contact"].map((e, i) => <div onClick={() => document.querySelector(["#section_"+i]).scrollIntoView() } className={"transition_0 corebox_x6 maxedcorebox_x6 row center btn_u " + (progress == i ? "fore_20" : "")}>{e}</div>)
+                            ["Home", "Projects", "About", "Experiences", "Contact"].map((e, i) => <div onClick={() => document.querySelector(["#section_"+i]).scrollIntoView() } className={"transition_0 corebox_x"+[6,6,6,8,7][i]+" maxedcorebox_x"+[6,6,6,8,7][i]+" row center btn_u " + (progress == i ? "fore_20" : "")}>{e}</div>)
                         }
                     </div>
                 </div>
